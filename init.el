02ef91cb8be494daa8f3afb2a9b4ee1f499258ec
Orrin Manning <omanning@vivafirst.com>
2022-07-06 21:50:49 -0500

    Do not use stable melpa repo

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
+
 (scroll-bar-mode -1)
 (require 'evil)
 (evil-mode 1)
